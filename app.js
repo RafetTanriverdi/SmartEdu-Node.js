8384d145ff43359dd62214f8c921ee127e8d4a91
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-06-05 23:11:00 +0300

    folder stracture

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+//templete engine 
+app.set("view engine", "ejs");
+
+//middleware
+app.use(express.static("public"));
+
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
